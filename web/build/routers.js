1745a91f671ca91c41e0e41fc2568979e7f35c90
yangyxu <945758912@qq.com>
2017-05-23 08:44:49 +0800

    update path

--- web/build/routers.js
+++ web/build/routers.js
@@ -1,13 +1,13 @@
 module.exports = zn.deepEachObject({
-			'/znadmin/Menu': './view/Menu.js',
-			'/znadmin/MyInfo': './view/MyInfo.js',
-			'/znadmin/Project': './view/Project.js',
-			'/znadmin/User': './view/User.js',
-			'/znadmin/UserInfo': './view/UserInfo.js',
-			'/znadmin/UserLog': './view/UserLog.js',
-			'/znadmin/UserForRoles': './view/UserForRoles.js',
-			'/znadmin/Role': './view/Role.js',
-			'/znadmin/Var': './view/Var.js'
+	'/znadmin/Menu': './view/Menu.js',
+	'/znadmin/MyInfo': './view/MyInfo.js',
+	'/znadmin/Project': './view/Project.js',
+	'/znadmin/User': './view/User.js',
+	'/znadmin/UserInfo': './view/UserInfo.js',
+	'/znadmin/UserLog': './view/UserLog.js',
+	'/znadmin/UserForRoles': './view/UserForRoles.js',
+	'/znadmin/Role': './view/Role.js',
+	'/znadmin/Var': './view/Var.js'
 }, function (value, index, item) {
-			return require(value);
+	return require(value);
 });
