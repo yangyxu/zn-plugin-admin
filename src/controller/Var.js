67b9e13f9f70d3368552e5fb7adafae3f128ba69
yangyxu <945758912@qq.com>
2020-04-28 18:33:59 +0800

    add rights api

--- src/controller/Var.js
+++ src/controller/Var.js
@@ -25,6 +25,37 @@ zn.define(function () {
                         });
                 }
             },
+            getByPidForRights: {
+                method: 'GET/POST',
+                argv: {
+                    pid: null,
+                    fields: 'id as value, zn_title as text'
+                },
+                value: function (request, response, chain){
+                    var _values = request.getValue();
+                    var _where = request.getValue("where");
+                    var _hasWhere = typeof _where=='object'?!!Object.keys(_where).length:_where;
+                    this.collection(MODEL)
+                        .select({
+                            fields: [
+                                'id', 'zn_title'
+                            ],
+                            order: {
+                                zn_create_time: 'desc'
+                            },
+                            where: [
+                                "zn_deleted=0" + (_hasWhere?(' and ' + zn.db.schema.SchemaSqlParser.parseWhere(_where, false)):''),
+                                ' and zn_tree_pid=' + _values.pid,
+                                ' and ' + zn.sql.rights()
+                            ]
+                        })
+                        .then(function (data){
+                            response.success(data);
+                        }, function (err){
+                            response.error(err);
+                        });
+                }
+            },
             getByPid: {
                 method: 'GET/POST',
                 argv: {
